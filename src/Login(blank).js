a6b57e440a896fa342162c0529890fb71f22016b
Jochem van Hout <jochemvanhout@gmail.com>
2020-07-01 10:30:45 +0200

    Mostly fixed the css + a blank login page
    Instructions on how to fill out the login page are found within.
    
    The most glaring issues with the css are fixed, just a few small details left.

---
+++ src/Login(blank).js
@@ -0,0 +1,12 @@
+import snoowrap from 'snoowrap';
+
+// Login to reddit bot account
+const r = new snoowrap({
+	userAgent: 'AdeptPop',
+	clientId: 'EpHJF-DZ7urT3w',
+	clientSecret: '2DL9N9JXlCHSFWwBhD5D2WFkQFI',
+	username: 'AdeptPop',
+	password: 'lolkaas'
+});
+
+export default r;
