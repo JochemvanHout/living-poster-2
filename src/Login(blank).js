feef4177da49ef71bcba66f565048ff7f1b314cf
Jochem van Hout <jochemvanhout@gmail.com>
2020-12-02 15:09:05 +0100

    Update Login(blank).js

--- src/Login(blank).js
+++ src/Login(blank).js
@@ -2,11 +2,11 @@ import snoowrap from 'snoowrap';
 
 // Login to reddit bot account
 const r = new snoowrap({
-	userAgent: 'AdeptPop',
-	clientId: 'EpHJF-DZ7urT3w',
-	clientSecret: '2DL9N9JXlCHSFWwBhD5D2WFkQFI',
-	username: 'AdeptPop',
-	password: 'lolkaas'
+	userAgent: '',
+	clientId: '',
+	clientSecret: '',
+	username: '',
+	password: ''
 });
 
 export default r;
